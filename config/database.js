edf58abee3b9c1dba0132bdad1a78850f4e67e46
Abraham Sotelo <asoteloa@outlook.com>
2017-07-03 21:21:41 -0500

    G400 reinstall, node and npm pachages update

--- config/database.js
+++ config/database.js
@@ -1,8 +1,8 @@
 'use strict';
 module.exports = {
   // DEP Development
-  //database : 'mongodb://localhost:27017/test',
+  database : 'mongodb://localhost:27017/test',
   // DEP Deploy
-  database : 'mongodb://abraham:1345@ds127531.mlab.com:27531/mean_tutorial',
+  //database : 'mongodb://abraham:1345@ds127531.mlab.com:27531/mean_tutorial',
   secret : 'yoursecret'
 }
