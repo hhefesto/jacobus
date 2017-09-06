8c6107e99770144623199c6b50168e54ad89f853
Abraham Sotelo <asoteloa@outlook.com>
2017-09-05 22:21:06 -0500

    Forgot to put Frontend in deployment mode

--- public/main.bundle.js
+++ public/main.bundle.js
@@ -1902,7 +1902,7 @@ var __metadata = (this && this.__metadata) || function (k, v) {
 };
 
 // true = Deploy, false = Development
-var deploy = false;
+var deploy = true;
 var DeployService = (function () {
     function DeployService() {
     }
