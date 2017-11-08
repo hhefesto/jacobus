a1f65d16a15584826834b8104ccdeb6c432a5926
hhefesto <daniel.herrera.rendon@gmail.com>
2017-11-08 16:30:19 -0600

    nueva versión que mandó Abraham

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,9 +1,10 @@
 ﻿'use strict';
 module.exports = {
   // DEP Development
-  database : 'mongodb://localhost:27017/test',
+  // database : 'mongodb://localhost:27017/test',
   // DEP Deploy
-  //database : 'mongodb://abraham:1345@ds127531.mlab.com:27531/mean_tutorial',
+    database : 'mongodb://abraham:1345@ds127531.mlab.com:27531/mean_tutorial',
+    //database : 'mongodb://<dbuser>:<dbpassword>@ds231725.mlab.com:31725/jacobus',
   secret : 'yoursecret'
 }
 
